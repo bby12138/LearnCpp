b5de64f238c92b8c9d3f8649d8ebaf003d4830ed
bby12138 <3134930866@qq.com>
2025-09-28 21:03:07 +0800

    Add files via upload

---
+++ 数据结构/List/main.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+#define eleType int
+
+struct ListNode
+{
+    ListNode *next;
+    eleType data;
+
+    ListNode(eleType x) : data(x), next(NULL) {}
+};
+
+class LinkedList
+{
+private:
+    ListNode *head;
+    int size;
+
+public:
+    LinkedList() : head(NULL), size(0) {}
+
+    ~LinkedList();
+
+    void insert(int i, eleType element);
+
+    void remove(int i);
+
+    ListNode *find(eleType value);
+
+    ListNode *get(int i);
+
+    void update(int i, eleType value);
+
+    void print();
+};
+
+LinkedList::~LinkedList()
+{
+    ListNode *curr = head;
+    while (curr != NULL)
+    {
+        ListNode *tmp = curr;
+        curr = curr->next;
+        delete tmp;
+    }
+}
+
+void LinkedList::insert(int i, int element)
+{
+    if (i < 0 || i > size)
+        throw std::out_of_range("Invalid position");
+
+    ListNode *newNode = new ListNode(element);
+    if (i == 0)
+    {
+        newNode->next = head;
+        head = newNode;
+    } else
+    {
+        ListNode *curr = head;
+        for (int j = 0; j < i - 1; ++j)
+            curr = curr->next;
+
+        newNode->next = curr->next;
+        curr->next = newNode;
+    }
+    ++size;
+}
+
+void LinkedList::remove(int i)
+{
+    if (i < 0 || i >= size)
+        throw std::out_of_range("Invalid position");
+    if (i == 0)
+    {
+        ListNode *temp = head;
+        head = head->next;
+        delete temp;
+    } else
+    {
+        ListNode *curr = head;
+        for (int j = 0; j < i - 1; ++j)
+            curr = curr->next;
+
+        ListNode *temp = curr->next;
+        curr->next = temp->next;
+        delete temp;
+    }
+    --size;
+}
+
+ListNode *LinkedList::find(eleType value)
+{
+    ListNode *curr = head;
+    while (curr && curr->data != value)
+        curr = curr->next;
+
+    return curr;
+}
+
+ListNode *LinkedList::get(int i)
+{
+    if (i < 0 || i >= size)
+        throw std::out_of_range("Invalid position");
+
+    ListNode *curr = head;
+    for (int j = 0; j < i; ++j)
+        curr = curr->next;
+
+    return curr;
+}
+
+void LinkedList::update(int i, eleType value)
+{
+    get(i)->data = value;
+}
+
+void LinkedList::print()
+{
+    ListNode *curr = head;
+    while (curr)
+    {
+        cout << curr->data << ' ';
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+
+int main()
+{
+    LinkedList list;
+
+    list.insert(0, 10);
+    list.insert(1, 20);
+    list.insert(1, 15);
+    cout << "After insertion: ";
+    list.print();
+
+    list.update(1, 18);
+    cout << "After update: ";
+    list.print();
+
+    ListNode *found = list.find(18);
+    if (found)
+        cout << "Found element: " << found->data << endl;
+    else
+        cout << "Element not found" << endl;
+
+    list.remove(1);
+    cout << "After deletion: ";
+    list.print();
+
+    ListNode *node = list.get(1);
+    cout << "Element at position 1: " << node->data << endl;
+
+    return 0;
+}
